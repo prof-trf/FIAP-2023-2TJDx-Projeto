8107883c7cffcea5e8e19fb486b41bc593564f7b
Thiago Filadelfo <thiago.filadelfo@gmail.com>
2023-04-26 09:56:27 -0300

    26abril

---
+++ app/src/main/java/br/com/fiap/edu/xboxone/LoginActivity.kt
@@ -0,0 +1,21 @@
+package br.com.fiap.edu.xboxone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.fiap.edu.xboxone.databinding.ActivityLoginBinding
+
+
+class LoginActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+    }
+}
