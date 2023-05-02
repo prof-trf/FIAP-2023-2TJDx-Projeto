604e3acd0bfe72935062c6044f7871f087a12104
Thiago Filadelfo <thiago.filadelfo@gmail.com>
2023-05-02 09:47:20 -0300

    02maio

--- app/src/main/java/br/com/fiap/edu/xboxone/LoginActivity.kt
+++ app/src/main/java/br/com/fiap/edu/xboxone/LoginActivity.kt
@@ -1,7 +1,10 @@
 package br.com.fiap.edu.xboxone
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import br.com.fiap.edu.xboxone.databinding.ActivityLoginBinding
 
 class LoginActivity: AppCompatActivity() {
@@ -16,5 +19,49 @@ class LoginActivity: AppCompatActivity() {
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
+
+        binding.edtEmail.addTextChangedListener {
+            if(it.toString().isNotEmpty()) {
+                binding.txtError.visibility = View.GONE
+            }
+        }
+
+        binding.btnVoltar.setOnClickListener {
+            finish()
+        }
+
+        binding.btnProximo.setOnClickListener {
+            val username = binding.edtEmail.text.toString()
+            if(validateUsername(username)) {
+                // chamar a proxima activity
+                val intent = Intent(this@LoginActivity, SenhaActivity::class.java)
+                intent.putExtra("username", username)
+                startActivity(intent)
+
+            } else {
+                binding.txtError.visibility = View.VISIBLE
+                binding.txtError.text = "Username inválido"
+            }
+        }
+    }
+
+    private fun validateUsername(username: String): Boolean {
+        // é um email
+        if(username.contains("@")) {
+            return true
+        }
+
+        // é um telefone
+        if(username.length == 11 &&
+            username.map { !it.isDigit() }.isEmpty()) {
+            return true
+        }
+
+        // é um skype
+        if(username.length >= 5 && username.isNotEmpty()) {
+            return true
+        }
+
+        return false
     }
 }
--- app/src/main/java/br/com/fiap/edu/xboxone/MainActivity.kt
+++ app/src/main/java/br/com/fiap/edu/xboxone/MainActivity.kt
@@ -1,8 +1,8 @@
 package br.com.fiap.edu.xboxone
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import br.com.fiap.edu.xboxone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnEntrar.setOnClickListener {
             val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            //intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
         }
     }
