8d9fc66aabef61d27acd801a677c826416d70784
Thiago Filadelfo <thiago.filadelfo@gmail.com>
2023-05-10 07:50:34 -0300

    03maio

--- app/src/main/java/br/com/fiap/edu/xboxone/LoginActivity.kt
+++ app/src/main/java/br/com/fiap/edu/xboxone/LoginActivity.kt
@@ -1,7 +1,12 @@
 package br.com.fiap.edu.xboxone
 
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.ColorFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.widget.addTextChangedListener
 import br.com.fiap.edu.xboxone.databinding.ActivityLoginBinding
 
 
@@ -17,5 +22,49 @@ class LoginActivity: AppCompatActivity() {
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
+
+        binding.btnVoltar.setOnClickListener { finish() }
+
+        binding.btnProximo.setOnClickListener {
+            val usuario = binding.edtEmail.text.toString()
+            val validadorUsuario = validarUsuario(usuario)
+            if(validadorUsuario) {
+                //vamos pra tela de senha
+                val intent = Intent(this@LoginActivity, SenhaActivity::class.java)
+                intent.putExtra("username", usuario)
+                startActivity(intent)
+            } else {
+                binding.edtEmail.backgroundTintList =
+                    AppCompatResources.getColorStateList(baseContext, R.color.red)
+            }
+        }
+
+        binding.edtEmail.addTextChangedListener {
+            val text = it.toString()
+            if(text.isNotEmpty()) {
+                binding.edtEmail.backgroundTintList =
+                    AppCompatResources.getColorStateList(baseContext, R.color.light_gray)
+            }
+        }
+    }
+
+    private fun validarUsuario(usuario: String): Boolean {
+        // eh e-mail
+        if(usuario.contains("@")) {
+            return true
+        }
+
+        // eh telefone
+        val procuraLetra = usuario.find { !it.isDigit() }
+        if(usuario.length == 11 && procuraLetra == null) {
+            return true
+        }
+
+        // eh skype
+        if(usuario.isNotEmpty() && usuario.length >= 5) {
+            return true
+        }
+
+        return false
     }
 }
