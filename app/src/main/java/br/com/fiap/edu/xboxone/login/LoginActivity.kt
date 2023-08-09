bfe1301f07e3abd3a00737d9d126531c5b26b678
Thiago Filadelfo <thiago.filadelfo@gmail.com>
2023-08-09 09:03:28 -0300

    08agosto

--- app/src/main/java/br/com/fiap/edu/xboxone/login/LoginActivity.kt
+++ app/src/main/java/br/com/fiap/edu/xboxone/login/LoginActivity.kt
@@ -1,88 +1,26 @@
 package br.com.fiap.edu.xboxone.login
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.widget.addTextChangedListener
 import br.com.fiap.edu.xboxone.R
-import br.com.fiap.edu.xboxone.databinding.ActivityLoginBinding
-import br.com.fiap.edu.xboxone.login.contrato.IValidacaoUsuarioView
-import br.com.fiap.edu.xboxone.senha.SenhaActivity
-import java.lang.Exception
 
 /* Declaração da tela de login */
-class LoginActivity : AppCompatActivity(), IValidacaoUsuarioView {
+class LoginActivity : AppCompatActivity() {
 
-    /*
-        Classe responsável de realizar a leitura do xml de tela
-        e prover o suporte para recuperar os componentes.
-    */
-    private lateinit var binding: ActivityLoginBinding
-
-    /* Classe controladora da tela de login */
-    private val controller = LoginController()
-
-    /* Primeiro método a ser executado quando montada a tela */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityLoginBinding.inflate(layoutInflater) /* realiza a leitura do xml */
-        setContentView(binding.root) /* marca a raiz do xml (layout) como interface para a tela */
+        setContentView(R.layout.activity_login)
     }
 
-    /* Método executado após a tela ser montada (lido o xml) */
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
 
-        setupBotaoVoltarUI() /* configura o componente botão voltar */
-        setupBotaoProximoUI() /* configura o componente botão proximo */
-        setupCaixaTextoEmailUI() /* configura o componente caixa de texto */
-    }
+        val loginFragment = LoginFragment()
 
-    private fun setupBotaoVoltarUI() {
-        /* setOnClickListener -> metodo usado para capturar o click do botão, no caso voltar */
-        binding.btnVoltar.setOnClickListener {
-            finish() /* fecha a tela, ou seja, retorna para a tela anterior */
-        }
-
-    }
-
-    private fun setupBotaoProximoUI() {
-        /* setOnClickListener -> metodo usado para capturar o click do botão, no caso proximo */
-        binding.btnProximo.setOnClickListener {
-            val usuario = binding.edtEmail.text.toString() /* recupera os dados da caixa de texto */
-            controller.validateUsername(usuario, this) /* metodo para validar o usuario digitado */
-        }
-    }
-
-    private fun setupCaixaTextoEmailUI() {
-        /* addTextChangedListener -> metodo usado para capturar os caracteres teclado pelo usuário */
-        binding.edtEmail.addTextChangedListener {
-            val text = it.toString() /* recupera os dados da caixa de texto */
-            if (controller.temEmail(text)) { /* valida se é um email */
-                binding.edtEmail.backgroundTintList =
-                    AppCompatResources.getColorStateList(baseContext, R.color.light_gray) /* altera a cor da linha da caixa de texto */
-            }
-        }
-    }
-
-    //// IValidacaoUsuarioView implementacao
-    override fun pesquisandoUsuario() {
-        binding.progressBar.visibility = View.VISIBLE /* aparece o progressbar */
-    }
-
-    override fun usuarioLocalizadoNaBaseDeDados(usuario: String) {
-        binding.progressBar.visibility = View.GONE /* esconde o progressbar */
-
-        val intent = SenhaActivity.navegar(this@LoginActivity, usuario) /* recupera a referencia para nevegar de tela */
-        startActivity(intent) /* realiza o start da tela que iremos navegar */
-    }
-
-    override fun erroNaPesquisaDaBaseDedados(error: Exception) {
-        binding.progressBar.visibility = View.GONE /* esconde o progressbar */
-
-        binding.edtEmail.backgroundTintList =
-            AppCompatResources.getColorStateList(baseContext, R.color.red)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.frl_conteudo, loginFragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/br/com/fiap/edu/xboxone/login/LoginFragment.kt
@@ -0,0 +1,123 @@
+package br.com.fiap.edu.xboxone.login
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import br.com.fiap.edu.xboxone.R
+import br.com.fiap.edu.xboxone.databinding.FragmentLoginBinding
+import br.com.fiap.edu.xboxone.login.contrato.IValidacaoUsuarioView
+import br.com.fiap.edu.xboxone.senha.SenhaActivity
+import java.lang.Exception
+
+class LoginFragment: Fragment(), IValidacaoUsuarioView {
+    /*
+        Classe responsável de realizar a leitura do xml de tela
+        e prover o suporte para recuperar os componentes.
+    */
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    /* Classe controladora da tela de login */
+    private val controller = LoginController()
+
+    private var countDownTimer: CountDownTimer? = null
+
+    /* Primeiro método a ser executado quando montada a tela */
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    /* Método executado após a tela ser montada (lido o xml) */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Toast.makeText(requireActivity(), "onPostCreate", Toast.LENGTH_SHORT).show()
+
+        setupBotaoVoltarUI() /* configura o componente botão voltar */
+        setupBotaoProximoUI() /* configura o componente botão proximo */
+        setupCaixaTextoEmailUI() /* configura o componente caixa de texto */
+
+        countDownTimer = object: CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                Log.d("onTick", millisUntilFinished.toString())
+            }
+
+            override fun onFinish() {
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(requireActivity(), "onResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(requireActivity(), "onPause", Toast.LENGTH_SHORT).show()
+        countDownTimer?.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+
+        Toast.makeText(requireActivity(), "onDestroy", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setupBotaoVoltarUI() {
+        /* setOnClickListener -> metodo usado para capturar o click do botão, no caso voltar */
+        binding.btnVoltar.setOnClickListener {
+            activity?.finish() /* fecha a tela, ou seja, retorna para a tela anterior */
+        }
+
+    }
+
+    private fun setupBotaoProximoUI() {
+        /* setOnClickListener -> metodo usado para capturar o click do botão, no caso proximo */
+        binding.btnProximo.setOnClickListener {
+            val usuario = binding.edtEmail.text.toString() /* recupera os dados da caixa de texto */
+            controller.validateUsername(usuario, this) /* metodo para validar o usuario digitado */
+        }
+    }
+
+    private fun setupCaixaTextoEmailUI() {
+        /* addTextChangedListener -> metodo usado para capturar os caracteres teclado pelo usuário */
+        binding.edtEmail.addTextChangedListener {
+            val text = it.toString() /* recupera os dados da caixa de texto */
+            if (controller.temEmail(text)) { /* valida se é um email */
+                binding.edtEmail.backgroundTintList =
+                    AppCompatResources.getColorStateList(requireContext(), R.color.light_gray) /* altera a cor da linha da caixa de texto */
+            }
+        }
+    }
+
+    //// IValidacaoUsuarioView implementacao
+    override fun pesquisandoUsuario() {
+        binding.progressBar.visibility = View.VISIBLE /* aparece o progressbar */
+    }
+
+    override fun usuarioLocalizadoNaBaseDeDados(usuario: String) {
+        binding.progressBar.visibility = View.GONE /* esconde o progressbar */
+
+        val intent = SenhaActivity.navegar(requireActivity(), usuario) /* recupera a referencia para nevegar de tela */
+        startActivity(intent) /* realiza o start da tela que iremos navegar */
+    }
+
+    override fun erroNaPesquisaDaBaseDedados(error: Exception) {
+        binding.progressBar.visibility = View.GONE /* esconde o progressbar */
+
+        binding.edtEmail.backgroundTintList =
+            AppCompatResources.getColorStateList(requireContext(), R.color.red)
+    }
+}
