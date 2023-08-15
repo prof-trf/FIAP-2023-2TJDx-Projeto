ad983b60c2a69edc0461673881a3d0d0fa4db7a0
Thiago Filadelfo <thiago.filadelfo@gmail.com>
2023-08-15 09:34:00 -0300

    15agosto - commit da alteraçao do login

--- app/src/main/java/br/com/fiap/edu/xboxone/senha/SenhaActivity.kt
+++ app/src/main/java/br/com/fiap/edu/xboxone/senha/SenhaActivity.kt
@@ -3,15 +3,15 @@ package br.com.fiap.edu.xboxone.senha
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
-import br.com.fiap.edu.xboxone.databinding.ActivitySenhaBinding
+import br.com.fiap.edu.xboxone.R
 
 /* Declaração da tela de login */
-class SenhaActivity : AppCompatActivity() {
+class SenhaActivity : AppCompatActivity(R.layout.activity_senha) {
+
     companion object {
         private const val USERNAME_PARAMS = "username"
+
         fun navegar(packageContext: Context, usuario: String): Intent {
             val intent = Intent(packageContext, SenhaActivity::class.java) /* configura a intent, tela onde estou e para qual irá navegar */
             intent.putExtra(USERNAME_PARAMS, usuario) /* adiciona um parametro para tela */
@@ -19,62 +19,16 @@ class SenhaActivity : AppCompatActivity() {
         }
     }
 
-    private lateinit var binding: ActivitySenhaBinding
-    private val controller = SenhaController()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivitySenhaBinding.inflate(layoutInflater) /* realiza a leitura do xml */
-        setContentView(binding.root) /* marca a raiz do xml (layout) como interface para a tela */
-    }
-
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
 
-        setupTextUsernameUI() /* configura o componente textview */
-        setupBotaoEntrarUI() /* configura o componente button */
-        setupBotaoVoltarUI() /* configura o componente button */
-        setupCaixaTextoSenhaUI() /* configura o componente caixa de texto */
-    }
+        val username = intent.getStringExtra(USERNAME_PARAMS) ?: ""
+        val senhaFragment = SenhaFragment.newInstance(username)
 
-    private fun setupTextUsernameUI() {
-        val username = intent.getStringExtra(USERNAME_PARAMS) /* recupera o parametro enviado da linha 17 */
-        binding.txtUsename.text = username /* marca na tela o texto do parametro recuperado */
-    }
-
-    private fun setupBotaoEntrarUI() {
-        /* setOnClickListener -> metodo usado para capturar o click do botão, no caso entrar */
-        binding.btnEntrar.setOnClickListener {
-            val senha = binding.edtSenha.text.toString() /* recupera os dados da caixa de texto */
-            if (controller.validateSenha(senha)) { /* metodo para validar a senha do usuario */
-                navegarParaTelaX()
-            } else {
-                binding.txtError.visibility = View.VISIBLE /* coloca o texto de erro visivel */
-                binding.txtError.text = "Digite uma senha válida" /* adiciona o texto quando ocorrer o erro */
-            }
-        }
-    }
-
-    private fun setupBotaoVoltarUI() {
-        /* setOnClickListener -> metodo usado para capturar o click do botão, no caso voltar */
-        binding.imvVoltar.setOnClickListener {
-            finish() /* fecha a tela, ou seja, retorna para a tela anterior */
-        }
-    }
-
-    private fun setupCaixaTextoSenhaUI() {
-        /* addTextChangedListener -> metodo usado para capturar os caracteres teclado pelo usuário */
-        binding.edtSenha.addTextChangedListener {
-            val senha = it.toString() /* recupera os dados da caixa de texto */
-            if (controller.temSenha(senha)) { /* metodo para validar a senha do usuario */
-                binding.txtError.visibility = View.GONE /* coloca o texto de erro invisivel e sem ocupar o tamanho na tela */
-            }
-        }
-    }
-
-    private fun navegarParaTelaX() {
-
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.frl_corpo, senhaFragment)
+            .commit()
     }
 
 }
---
+++ app/src/main/java/br/com/fiap/edu/xboxone/senha/SenhaFragment.kt
@@ -0,0 +1,86 @@
+package br.com.fiap.edu.xboxone.senha
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import br.com.fiap.edu.xboxone.databinding.FragmentSenhaBinding
+
+/* Declaração da tela de login */
+class SenhaFragment : Fragment {
+    companion object {
+        private const val USERNAME_PARAMS = "username"
+        fun newInstance(usuario: String): Fragment {
+            val senhaFragment = SenhaFragment()
+            senhaFragment.arguments?.putString(USERNAME_PARAMS, usuario) /* adiciona um parametro para tela */
+            return senhaFragment
+        }
+    }
+
+    private constructor()
+
+    private var _binding: FragmentSenhaBinding? = null
+    private val binding get() = _binding!!
+
+    private val controller = SenhaController()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSenhaBinding.inflate(inflater, container, false) /* realiza a leitura do xml */
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupTextUsernameUI() /* configura o componente textview */
+        setupBotaoEntrarUI() /* configura o componente button */
+        setupBotaoVoltarUI() /* configura o componente button */
+        setupCaixaTextoSenhaUI() /* configura o componente caixa de texto */
+    }
+
+    private fun setupTextUsernameUI() {
+        val username = arguments?.getString(USERNAME_PARAMS) /* recupera o parametro enviado da linha 17 */
+        binding.txtUsename.text = username /* marca na tela o texto do parametro recuperado */
+    }
+
+    private fun setupBotaoEntrarUI() {
+        /* setOnClickListener -> metodo usado para capturar o click do botão, no caso entrar */
+        binding.btnEntrar.setOnClickListener {
+            val senha = binding.edtSenha.text.toString() /* recupera os dados da caixa de texto */
+            if (controller.validateSenha(senha)) { /* metodo para validar a senha do usuario */
+                navegarParaTelaX()
+            } else {
+                binding.txtError.visibility = View.VISIBLE /* coloca o texto de erro visivel */
+                binding.txtError.text = "Digite uma senha válida" /* adiciona o texto quando ocorrer o erro */
+            }
+        }
+    }
+
+    private fun setupBotaoVoltarUI() {
+        /* setOnClickListener -> metodo usado para capturar o click do botão, no caso voltar */
+        binding.imvVoltar.setOnClickListener {
+            activity?.finish() /* fecha a tela, ou seja, retorna para a tela anterior */
+        }
+    }
+
+    private fun setupCaixaTextoSenhaUI() {
+        /* addTextChangedListener -> metodo usado para capturar os caracteres teclado pelo usuário */
+        binding.edtSenha.addTextChangedListener {
+            val senha = it.toString() /* recupera os dados da caixa de texto */
+            if (controller.temSenha(senha)) { /* metodo para validar a senha do usuario */
+                binding.txtError.visibility = View.GONE /* coloca o texto de erro invisivel e sem ocupar o tamanho na tela */
+            }
+        }
+    }
+
+    private fun navegarParaTelaX() {
+
+    }
+
+}
